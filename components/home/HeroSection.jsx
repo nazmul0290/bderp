f82222d878e12d93ebffbbcbb79dfb95711aa1f6
Taijul Islam <taijulislam.st9@gmail.com>
2023-01-27 23:09:59 +0600

    feat(hero section): desig

--- components/home/HeroSection.jsx
+++ components/home/HeroSection.jsx
@@ -1,37 +1,36 @@
 import Image from "next/image";
 import React from "react";
+import { Grid } from "@mui/material";
 
 const HeroSection = () => {
   return (
-    <section className="bg-cover">
-      <div className=" w-full min-h-[600px] bg-cover rectanglePolygon">
-        <div></div>
-        <div></div>
+    <section className="relative min-h-[690px] w-full  mb-32  ">
+      <div
+        className="relative min-h-[465px] w-full bg-[length:100%_100%]  bg-no-repeat bg-top bg-origin-border"
+        style={{
+          backgroundImage: "url('/img/home/banner-bg.png')",
+        }}
+      >
+        <div className="inline-flex justify-end items-center absolute top-[90px] left-0 right-0 h-[644px]">
+          <div
+            className="w-1/2 h-full bg-[length:auto_100%] bg-no-repeat"
+            style={{
+              backgroundImage: "url('/img/home/banner-right-bg.png')",
+            }}
+          ></div>
+        </div>
       </div>
-      <div className="container px-4 py-20 mx-auto">
-        <div className="flex items-center justify-between">
-          <div className="w-full md:w-1/2">
-            <div>
-              <p className="text-gray-900 md:text-4xl sm:text-xl">
-                {" "}
-                Let Us Help You To .{" "}
-              </p>
-              <p className="md:text-4xl text-2xl font-bold text-[#417ee7] drop-shadow-lg leading-10 mt-2">
-                Grow Your Business!
-              </p>
-              <p className="mt-2 font-medium text-gray-600">
+      <div className="absolute top-0 bottom-0 left-0 right-0 z-10 h-full mx-auto">
+        <div className="flex h-full justify-center items-center relative top-[90px] container">
+          <div className="grid grid-cols-2  h-[calc(100% - 85px)] ">
+            <div className="w-full my-auto">
+              <h1 className="font-semibold text-primary text-[64px] leading-[77px]">
+                Let Us Help You To Grow Your Business!
+              </h1>
+              <p className="leading-5 mt-9 text-primary ">
                 Simply Save Time & Money With BD ERP
               </p>
             </div>
-          </div>
-          <div className="self-center hidden w-1/2 lg:block">
-            <Image
-              src="/img/home/Hero-Image.png"
-              alt="Hero-Image"
-              className="w-full"
-              width={700}
-              height={700}
-            />
           </div>
         </div>
       </div>
